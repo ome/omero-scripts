bf19c618b81b42f2a3fe07bcbbcee8ecc7ab4594
Sebastien Besson <seb.besson@gmail.com>
2013-09-20 16:19:37 +0100

    Fix flake8 output for FLIM_initialise script

--- omero/setup_scripts/FLIM_initialise.py
+++ omero/setup_scripts/FLIM_initialise.py
@@ -15,14 +15,15 @@
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License along
   with this program; if not, write to the Free Software Foundation, Inc.,
   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 ------------------------------------------------------------------------------
 
-Initialises the namespace and keywords for the FLIM script, /scripts/omero/analysis_scripts/FLIM.py
+Initialises the namespace and keywords for the FLIM script,
+/scripts/omero/analysis_scripts/FLIM.py
 
 @author  Pieta Schofield &nbsp;&nbsp;&nbsp;&nbsp;
 <a href="mailto:p@schofield.dundee.ac.uk">will@lifesci.dundee.ac.uk</a>
@@ -33,48 +34,44 @@ Initialises the namespace and keywords for the FLIM script, /scripts/omero/analy
 (<b>Internal version:</b> $Revision: $Date: $)
 </small>
 @since 3.0-Beta4.1
- 
+
 """
 
-# OMERO Imports 
+# OMERO Imports
 import omero.clients
 import omero.scripts as scripts
-import omero.util.pixelstypetopython as pixelstypetopython
-from omero.rtypes import *
-from omero.util.OmeroPopo import EllipseData as EllipseData
-from omero.util.OmeroPopo import RectData as RectData
-from omero.util.OmeroPopo import MaskData as MaskData
-from omero.util.OmeroPopo import WorkflowData as WorkflowData
-from omero.util.OmeroPopo import ROIData as ROIData
-from omero.util.OmeroPopo import ROICoordinate as ROICoordinate
 
 
 # Script Utility helper methods.
 import omero.util.script_utils as script_utils
 
-CELL = omero.constants.analysis.flim.KEYWORDFLIMCELL;
-NAMESPACE = omero.constants.analysis.flim.NSFLIM;
-BACKGROUND = omero.constants.analysis.flim.KEYWORDFLIMBACKGROUND;
+CELL = omero.constants.analysis.flim.KEYWORDFLIMCELL
+NAMESPACE = omero.constants.analysis.flim.NSFLIM
+BACKGROUND = omero.constants.analysis.flim.KEYWORDFLIMBACKGROUND
+
 
 def initialise(session):
-    iQuery = session.getQueryService();
-    iUpdate = session.getUpdateService();
-    
+    iQuery = session.getQueryService()
+    iUpdate = session.getUpdateService()
+
     #keywords = BACKGROUND+","+CELL;
-    keywords = CELL;
-    script_utils.registerNamespace(iQuery, iUpdate, NAMESPACE, keywords);
+    keywords = CELL
+    script_utils.registerNamespace(iQuery, iUpdate, NAMESPACE, keywords)
+
 
 def runAsScript():
-    client = scripts.client('FLIM_initialise.py', """Sets up the namespace and keywords for the FLIM script.""",
-    version = "4.2.0",
-    authors = ["Donald MacDonald", "OME Team"],
-    institutions = ["University of Dundee"],
-    contact = "ome-users@lists.openmicroscopy.org.uk",)
+    client = scripts.client(
+        'FLIM_initialise.py',
+        """Sets up the namespace and keywords for the FLIM script.""",
+        version="4.2.0",
+        authors=["Donald MacDonald", "OME Team"],
+        institutions=["University of Dundee"],
+        contact="ome-users@lists.openmicroscopy.org.uk",)
     try:
-        session = client.getSession();
+        session = client.getSession()
         initialise(session)
     finally:
         client.closeSession()
 
 if __name__ == '__main__':
-    runAsScript();
+    runAsScript()
