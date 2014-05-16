51091ce7eac85dc390812ea97cc3ad0f8238c65c
Sebastien Besson <seb.besson@gmail.com>
2014-05-16 10:00:14 +0100

    Bump copyright headers

--- omero/util_scripts/Images_From_ROIs.py
+++ omero/util_scripts/Images_From_ROIs.py
@@ -4,7 +4,7 @@
  components/tools/OmeroPy/scripts/omero/util_scripts/Images_From_ROIs.py
 
 -----------------------------------------------------------------------------
-  Copyright (C) 2006-2010 University of Dundee. All rights reserved.
+  Copyright (C) 2006-2014 University of Dundee. All rights reserved.
 
 
   This program is free software; you can redistribute it and/or modify
