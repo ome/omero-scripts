ed99a12190d62dc20c5db1649b541b2b08e45baa
jmoore <josh@glencoesoftware.com>
2019-09-02 12:09:09 +0200

    Migrate testParseAllOfficialScripts here
    see:
     - https://github.com/ome/omero-py/pull/10#pullrequestreview-281971929

---
+++ test/unit/test_parse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+   Test of the omero.scripts.parse functionality
+   Copyright 2009-2019 Open Microscopy Environment. All rights reserved.
+   Use is subject to license terms supplied in LICENSE.txt
+"""
+
+
+from path import path
+from omero.scripts import parse_file
+
+
+SCRIPTS = path(".") / ".." / "omero"
+
+
+class TestParse(object):
+
+    def test_parse_all_official_scripts(self):
+        for script in SCRIPTS.walk("*.py"):
+            try:
+                parse_file(str(script))
+            except Exception, e:
+                assert False, "%s\n%s" % (script, e)
