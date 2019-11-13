d8aa6221df52e5254c2a29fa90018c52bad438eb
William Moore <w.moore@dundee.ac.uk>
2019-11-13 23:54:50 +0000

    Fix Thumbnail_Figure script

--- omero/figure_scripts/Thumbnail_Figure.py
+++ omero/figure_scripts/Thumbnail_Figure.py
@@ -33,19 +33,24 @@ project.
 @since 3.0
 
 """
+from io import BytesIO
 import omero.scripts as scripts
 from omero.gateway import BlitzGateway
 import omero.util.script_utils as script_utils
-from omero.rtypes import rlong, rstring, robject
+from omero.rtypes import rlong, rstring, robject, rint
 import omero.util.image_utils as image_utils
 from omero.constants.namespaces import NSCREATED
 import os
 
 try:
-    from PIL import Image, ImageDraw  # see ticket:2597
+    from PIL import Image, ImageDraw, ImageFont
 except ImportError:
     import Image
     import ImageDraw  # see ticket:2597
+    import ImageFont
+
+from omero.gateway import THISPATH as GATEWAYPATH
+
 
 WHITE = (255, 255, 255)
 
@@ -57,11 +62,160 @@ def log(text):
     Adds lines of text to the log_lines list, so they can be collected into a
     figure legend.
     """
-    try:
-        text = text.encode('utf8')
-    except UnicodeEncodeError:
-        pass
     log_lines.append(text)
+
+
+def paste_image(image, canvas, x, y):
+    """
+    Pastes the image onto the canvas at the specified coordinates
+    Image and canvas are instances of PIL 'Image'
+
+    @param image:		The PIL image to be pasted. Image
+    @param canvas:		The PIL image on which to paste. Image
+    @param x:			X coordinate (left) to paste
+    @param y: 			Y coordinate (top) to paste
+    """
+
+    x = int(x)
+    y = int(y)
+    x_right = image.size[0] + x
+    y_bottom = image.size[1] + y
+    # make a tuple of topleft-x, topleft-y, bottomRight-x, bottomRight-y
+    pastebox = (x, y, x_right, y_bottom)
+    canvas.paste(image, pastebox)
+
+
+def get_font(fontsize):
+    """
+    Returns a PIL ImageFont Sans-serif true-type font of the specified size
+    or a pre-compiled font of fixed size if the ttf font is not found
+
+    @param fontsize:	The size of the font you want
+    @return: 	A PIL Font
+    """
+
+    font_path = os.path.join(GATEWAYPATH, "pilfonts", "FreeSans.ttf")
+    try:
+        font = ImageFont.truetype(font_path, fontsize)
+    except:
+        font = ImageFont.load('%s/pilfonts/B%0.2d.pil' % (GATEWAYPATH, 24))
+    return font
+
+
+def paint_thumbnail_grid(thumbnail_store, length, spacing, pixel_ids,
+                         col_count, bg=(255, 255, 255), left_label=None,
+                         text_color=(0, 0, 0), fontsize=None, top_label=None):
+    """
+    Retrieves thumbnails for each pixelId, and places them in a grid,
+    with White background.
+    Option to add a vertical label to the left of the canvas
+    Creates a PIL 'Image' which is returned
+
+    @param thumbnail_store:  The omero thumbnail store.
+    @param length:			 Length of longest thumbnail side, int
+    @param spacing:			 The spacing between thumbnails and around the
+                             edges. int
+    @param pixel_ids:		 List of pixel IDs. [long]
+    @param col_count:		 The number of columns. int
+    @param bg:				 Background colour as (r,g,b).
+                             Default is white (255, 255, 255)
+    @param left_label: 		 Optional string to display vertically to the left.
+    @param text_color:		 The color of the text as (r,g,b).
+                             Default is black (0, 0, 0)
+    @param fontsize:		 Size of the font.
+                             Default is calculated based on thumbnail length,
+                             int
+    @return: 			    The PIL Image canvas.
+    """
+    mode = "RGB"
+    # work out how many rows and columns are needed for all the images
+    img_count = len(pixel_ids)
+
+    row_count = img_count // col_count
+    # check that we have enough rows and cols...
+    while (col_count * row_count) < img_count:
+        row_count += 1
+
+    left_space = top_space = spacing
+    min_width = 0
+
+    text_height = 0
+    if left_label or top_label:
+        # if no images (no rows), need to make at least one row to show label
+        if left_label is not None and row_count == 0:
+            row_count = 1
+        if fontsize is None:
+            fontsize = (length // 10) + 5
+        font = get_font(fontsize)
+        if left_label:
+            text_width, text_height = font.getsize(left_label)
+            left_space = spacing + text_height + spacing
+        if top_label:
+            text_width, text_height = font.getsize(top_label)
+            top_space = spacing + text_height + spacing
+            min_width = left_space + text_width + spacing
+
+    # work out the canvas size needed, and create a white canvas
+    cols_needed = min(col_count, img_count)
+    v = left_space + cols_needed * (length + spacing)
+    canvas_width = int(max(min_width, v))
+    canvas_height = int(top_space + row_count * (length + spacing) + spacing)
+    mode = "RGB"
+    size = (canvas_width, canvas_height)
+    canvas = Image.new(mode, size, bg)
+
+    # to write text up the left side, need to write it on horizontal canvas
+    # and rotate.
+    if left_label:
+        label_canvas_width = canvas_height
+        label_canvas_height = text_height + spacing
+        label_size = (label_canvas_width, label_canvas_height)
+        text_canvas = Image.new(mode, label_size, bg)
+        draw = ImageDraw.Draw(text_canvas)
+        text_width = font.getsize(left_label)[0]
+        text_x = (label_canvas_width - text_width) // 2
+        draw.text((text_x, spacing), left_label, font=font, fill=text_color)
+        vertical_canvas = text_canvas.rotate(90)
+        paste_image(vertical_canvas, canvas, 0, 0)
+        del draw
+
+    if top_label is not None:
+        label_canvas_width = canvas_width
+        label_canvas_height = text_height + spacing
+        label_size = (label_canvas_width, label_canvas_height)
+        text_canvas = Image.new(mode, label_size, bg)
+        draw = ImageDraw.Draw(text_canvas)
+        draw.text((spacing, spacing), top_label, font=font, fill=text_color)
+        paste_image(text_canvas, canvas, left_space, 0)
+        del draw
+
+    # loop through the images, getting a thumbnail and placing it on a new row
+    # and column
+    r = 0
+    c = 0
+    thumbnail_map = thumbnail_store.getThumbnailByLongestSideSet(rint(length),
+                                                                 pixel_ids)
+    for pixels_id in pixel_ids:
+        if pixels_id in thumbnail_map:
+            thumbnail = thumbnail_map[pixels_id]
+            # check we have a thumbnail (won't get one if image is invalid)
+            if thumbnail:
+                # make an "Image" from the string-encoded thumbnail
+                thumb_image = Image.open(BytesIO(thumbnail))
+                # paste the image onto the canvas at the correct coordinates
+                # for the current row and column
+                x = c * (length + spacing) + left_space
+                y = r * (length + spacing) + top_space
+                paste_image(thumb_image, canvas, x, y)
+
+        # increment the column, and if we're at the last column, start a new
+        # row
+        c = c + 1
+        if c == col_count:
+            c = 0
+            r = r + 1
+
+    return canvas
 
 
 def sort_images_by_tag(tag_ids, img_tags):
@@ -237,7 +391,7 @@ def paint_dataset_canvas(conn, images, title, tag_ids=None,
                 % (tag_string, len(tagset_pix_ids)))
             if not show_subset_labels:
                 tag_string = None
-            sub_canvas = image_utils.paint_thumbnail_grid(
+            sub_canvas = paint_thumbnail_grid(
                 thumbnail_store, length,
                 spacing, tagset_pix_ids, col_count, top_label=tag_string)
             tag_sub_panes.append(sub_canvas)
@@ -282,7 +436,7 @@ def paint_dataset_canvas(conn, images, title, tag_ids=None,
             p_x = left_spacer
             p_y = 0
             for pane in tag_sub_panes:
-                image_utils.paste_image(pane, tag_canvas, p_x, p_y)
+                paste_image(pane, tag_canvas, p_x, p_y)
                 p_y += pane.size[1]
             if tag_text is not None:
                 draw = ImageDraw.Draw(tag_canvas)
@@ -301,7 +455,7 @@ def paint_dataset_canvas(conn, images, title, tag_ids=None,
         for image_id in ds_image_ids:
             log("  Name: %s  ID: %d" % (image_names[image_id], image_id))
             pixel_ids.append(image_pixel_map[image_id])
-        fig_canvas = image_utils.paint_thumbnail_grid(
+        fig_canvas = paint_thumbnail_grid(
             thumbnail_store, length, spacing, pixel_ids, col_count)
         tag_panes.append(fig_canvas)
 
@@ -310,12 +464,12 @@ def paint_dataset_canvas(conn, images, title, tag_ids=None,
     max_width = max([c.size[0] for c in tag_panes])
     total_height = total_height + sum([c.size[1]+tagset_spacer
                                       for c in tag_panes]) - tagset_spacer
-    size = (max_width, total_height)
+    size = (int(max_width), int(total_height))
     full_canvas = Image.new(mode, size, WHITE)
     p_x = 0
     p_y = top_spacer
     for pane in tag_panes:
-        image_utils.paste_image(pane, full_canvas, p_x, p_y)
+        paste_image(pane, full_canvas, p_x, p_y)
         p_y += pane.size[1] + tagset_spacer
 
     # create dates for the image timestamps. If dates are not the same, show
@@ -399,7 +553,11 @@ def make_thumbnail_figure(conn, script_params):
             log("Dataset: %s     ID: %d"
                 % (dataset.getName(), dataset.getId()))
             images = list(dataset.listChildren())
-            title = dataset.getName().decode('utf8')
+            title = dataset.getName()
+            try:
+                title = title.decode('utf8')
+            except AttributeError:
+                pass    # python 3
             ds_canvas = paint_dataset_canvas(
                 conn, images, title, tag_ids, show_untagged,
                 length=thumb_size, col_count=max_columns)
@@ -423,7 +581,7 @@ def make_thumbnail_figure(conn, script_params):
     figure = Image.new("RGB", (fig_width, fig_height), WHITE)
     y = 0
     for ds in ds_canvases:
-        image_utils.paste_image(ds, figure, 0, y)
+        paste_image(ds, figure, 0, y)
         y += ds.size[1]
 
     log("")
