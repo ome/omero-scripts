a2cfae05045a8ee73d6fe056837647dc5f643c92
Jean-Marie Burel <j.burel@dundee.ac.uk>
2020-01-07 12:07:25 +0000

    add classifiers and require py3

--- setup.py
+++ setup.py
@@ -65,6 +65,14 @@ class PyTest(test_command):
         sys.exit(errno)
 
 
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
 version = '5.6.dev2'
 url = "https://github.com/ome/omero-scripts/"
 
@@ -78,7 +86,21 @@ setup(
         'omero.import_scripts',
         'omero.util_scripts'],
     description="OMERO scripts",
-    long_description="OMERO scripts",
+    long_description=read('README.rst'),
+    classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Plugins',
+          'Intended Audience :: Developers',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU General Public License v2 '
+          'or later (GPLv2+)',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],  # Get strings from
+          # http://pypi.python.org/pypi?%3Aaction=list_classifiers
     author='The Open Microscopy Team',
     author_email='ome-devel@lists.openmicroscopy.org.uk',
     license='GPL-2.0+',
@@ -86,5 +108,6 @@ setup(
     zip_safe=False,
     download_url='%s' % url,
     cmdclass={'test': PyTest},
+    python_requires='>=3',
     tests_require=['pytest'],
 )
