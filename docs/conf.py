3da40b0000f6cb578fc968f1e3b74b6f3425f475
William Moore <w.moore@dundee.ac.uk>
2020-10-15 15:40:30 +0100

    Add docs/analysis_scripts.rst

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ master_doc = 'index'
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-sys.path.insert(0, os.path.abspath('../omero/'))
+sys.path.insert(0, os.path.abspath('../omero/analysis_scripts/'))
 
 
 # -- Project information -----------------------------------------------------
