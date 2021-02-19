9e68f3c34221ca183ef2ea5cad4da4003aa4c364
Jean-Marie Burel <j.burel@dundee.ac.uk>
2021-02-19 16:21:10 +0000

    review conf

--- docs/conf.py
+++ docs/conf.py
@@ -28,8 +28,8 @@ sys.path.insert(0, os.path.abspath('../omero/util_scripts'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'will-moore-omero-scripts'
-copyright = '2020, OME'
+project = 'jburel-omero-scripts'
+copyright = '2021, OME'
 author = 'OME'
 
 # The full version, including alpha/beta/rc tags
@@ -67,3 +67,52 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title,
+#  author, documentclass [howto, manual, or own class]).
+latex_documents = [
+    (master_doc, 'OMEROROIs.tex', u'OMERO ROIs Documentation',
+     u'Open Microscopy Environment', 'manual'),
+]
+
+
+# -- Options for manual page output ------------------------------------------
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    (master_doc, 'omeroscripts', u'OMERO Scripts Documentation',
+     [author], 1)
+]
+
+
+# -- Options for Texinfo output ----------------------------------------------
+
+# Grouping the document tree into Texinfo files. List of tuples
+# (source start file, target name, title, author,
+#  dir menu entry, description, category)
+texinfo_documents = [
+    (master_doc, 'OMEROScripts', u'OMERO Script Documentation',
+     author, 'OMEROScripts', 'One line description of project.',
+     'Miscellaneous'),
+]
+
+
+# -- Options for Epub output -------------------------------------------------
+
+# Bibliographic Dublin Core info.
+epub_title = project
+
+# The unique identifier of the text. This can be a ISBN number
+# or the project homepage.
+#
+# epub_identifier = ''
+
+# A unique identification for the text.
+#
+# epub_uid = ''
+
+# A list of files that should not be packed into the epub file.
+epub_exclude_files = ['search.html']
+
