8120f10bfba5ef71603bb5be766d47e46d7568a4
Rémy Dornier <remy.dornier@epfl.ch>
2023-07-12 14:52:54 +0200

    Clean the code and make it more pythonish

--- omero/annotation_scripts/Add_owner_as_key_value.py
+++ omero/annotation_scripts/Add_owner_as_key_value.py
@@ -19,22 +19,18 @@
 Created by Rémy Dornier, based on Christian Evenhuis work.
 """
 
-
 import omero
 from omero.gateway import BlitzGateway
-from omero.rtypes import rstring, rlong
+from omero.rtypes import rstring
 import omero.scripts as scripts
-#from omero.cmd import Delete2
 from datetime import date
-
-import sys
 import copy
-
 from collections import OrderedDict
 
 
 def get_existing_map_annotations(obj, namespace):
     """Get all Map Annotations linked to the object"""
+
     ord_dict = OrderedDict()
     for ann in obj.listAnnotations(ns=namespace):
         if isinstance(ann, omero.gateway.MapAnnotationWrapper):
@@ -45,27 +41,14 @@ def get_existing_map_annotations(obj, namespace):
                 ord_dict[k].add(v)
     return ord_dict
 
-'''
-def remove_map_annotations(conn, object, namespace):
-    """Remove ALL Map Annotations on the object"""
-    anns = list(object.listAnnotations(ns=namespace))
-    mapann_ids = [ann.id for ann in anns
-                  if isinstance(ann, omero.gateway.MapAnnotationWrapper)]
 
-    try:
-        delete = Delete2(targetObjects={'MapAnnotation': mapann_ids})
-        handle = conn.c.sf.submit(delete)
-        conn.c.waitOnCmd(handle, loops=10, ms=500, failonerror=True,
-                         failontimeout=False, closehandle=False)
-
-    except Exception as ex:
-        print("Failed to delete links: {}".format(ex.message))
-    return
-'''
-
-"""Add a key value pair to the object"""
-"""Code taken and adapted from Christian Evenhuis,  https://github.com/ome/omero-scripts/blob/develop/omero/annotation_scripts/KeyVal_from_csv.py"""
 def annotate_object(conn, obj, key, val):
+    """
+    Add a key value pair to the object
+    Code taken and adapted from Christian Evenhuis,
+    https://github.com/ome/omero-scripts/blob/develop/omero/annotation_scripts/KeyVal_from_csv.py
+    """
+
     obj_updated = False
     namespace = "Previous Owners"
     existing_kv = get_existing_map_annotations(obj, namespace)
@@ -87,11 +70,6 @@ def annotate_object(conn, obj, key, val):
         try:
             # convert the ordered dict to a list of lists
             kv_list = []
-
-            # if existing_kv is not None:
-            #     for k, vset in existing_kv.items():
-            #         for v in vset:
-            #             kv_list.append([k, v])
 
             for k, vset in updated_kv.items():
                 for v in vset:
@@ -117,126 +95,133 @@ def annotate_object(conn, obj, key, val):
             obj_updated = True
 
         except omero.SecurityViolation:
-            print("You do not have the right to write annotations for ",obj.OMERO_CLASS, "", obj.getId())
+            print("You do not have the right to write annotations for ", obj.OMERO_CLASS, "", obj.getId())
     else:
         print("No change change in kv")
 
     return obj_updated
 
 
-"""
-Add the owner as key value pair to an image
-return 1 if owner has been added, 0 otherwise
-"""
 def process_image(conn, image, key, owner):
-    return (1 if annotate_object(conn, image, key, owner)== True else 0)
+    """
+    Add the owner as key value pair to an image
+    return 1 if owner has been added, 0 otherwise
+    """
+
+    return 1 if annotate_object(conn, image, key, owner) is True else 0
 
 
-"""
-Add the owner as key value pair to a dataset and its children
-return the number of processed images
-"""
 def process_dataset(conn, dataset, key, owner):
-    nImage = 0
+    """
+    Add the owner as key value pair to a dataset and its children
+    return the number of processed images
+    """
+
+    n_image = 0
     for image in dataset.listChildren():
-        nImage += process_image(conn, image, key, owner)
+        n_image += process_image(conn, image, key, owner)
 
-    return nImage, (1 if annotate_object(conn, dataset, key, owner)== True else 0)
+    return n_image, (1 if annotate_object(conn, dataset, key, owner) is True else 0)
 
 
-"""
-Add the owner as key value pair to a project and its children
-return the number of processed images & datasets
-"""
 def process_project(conn, project, key, owner):
-    nDataset = 0
-    nImage = 0
+    """
+    Add the owner as key value pair to a project and its children
+    return the number of processed images & datasets
+    """
+
+    n_dataset = 0
+    n_image = 0
     for dataset in project.listChildren():
-        nImageTmp, nDatasetTmp = process_dataset(conn, dataset, key, owner)
-        nDataset += nDatasetTmp
-        nImage += nImageTmp
+        n_image_tmp, n_dataset_tmp = process_dataset(conn, dataset, key, owner)
+        n_dataset += n_dataset_tmp
+        n_image += n_image_tmp
 
-    return nImage, nDataset, (1 if annotate_object(conn, project, key, owner)== True else 0)
+    return n_image, n_dataset, (1 if annotate_object(conn, project, key, owner) is True else 0)
 
 
-"""
-Add the owner as key value pair to a well and its children
-return the number of processed images 
-"""
 def process_well(conn, well, key, owner):
-    nImage = 0
+    """
+    Add the owner as key value pair to a well and its children
+    return the number of processed images
+    """
+
+    n_image = 0
     for wellSample in well.listChildren():
-        nImage += process_image(conn, wellSample.getImage(), key, owner)
+        n_image += process_image(conn, wellSample.getImage(), key, owner)
 
-    return nImage, (1 if annotate_object(conn, well, key, owner)== True else 0)
+    return n_image, (1 if annotate_object(conn, well, key, owner) is True else 0)
 
 
-"""
-Add the owner as key value pair to a plate and its children
-return the number of processed images & wells
-"""
 def process_plate(conn, plate, key, owner):
-    nImage = 0
-    nWell = 0
+    """
+    Add the owner as key value pair to a plate and its children
+    return the number of processed images & wells
+    """
+
+    n_image = 0
+    n_well = 0
     for well in plate.listChildren():
-        nImageTmp, nWellTmp = process_well(conn, well, key, owner)
-        nWell += nWellTmp
-        nImage += nImageTmp
+        n_image_tmp, n_well_tmp = process_well(conn, well, key, owner)
+        n_well += n_well_tmp
+        n_image += n_image_tmp
 
-    return nImage, nWell, (1 if annotate_object(conn, plate, key, owner)== True else 0)
+    return n_image, n_well, (1 if annotate_object(conn, plate, key, owner) is True else 0)
 
 
-"""
-Add the owner as key value pair to a screen and its children
-return the number of processed images, wells & plates
-"""
 def process_screen(conn, screen, key, owner):
-    nImage = 0
-    nWell = 0
-    nPlate = 0
+    """
+    Add the owner as key value pair to a screen and its children
+    return the number of processed images, wells & plates
+    """
+
+    n_image = 0
+    n_well = 0
+    n_plate = 0
     for plate in screen.listChildren():
-        nImageTmp, nWellTmp, nPlateTmp = process_plate(conn, plate, key, owner)
-        nImage += nImageTmp
-        nWell += nWellTmp
-        nPlate += nPlateTmp
+        n_image_tmp, n_well_tmp, n_plate_tmp = process_plate(conn, plate, key, owner)
+        n_image += n_image_tmp
+        n_well += n_well_tmp
+        n_plate += n_plate_tmp
 
-    return nImage, nWell, nPlate, (1 if annotate_object(conn, screen, key, owner)== True else 0)
+    return n_image, n_well, n_plate, (1 if annotate_object(conn, screen, key, owner) is True else 0)
 
 
-"""
-Get the given container(s) or given experimenter(s) and scan all their children to add 
-data owner as a key-value pair to all of them.
-"""
 def add_owner_as_keyval(conn, script_params):
+    """
+    Get the given container(s) or given experimenter(s) and scan all their children to add
+    data owner as a key-value pair to all of them.
+    """
+
     # select the object type (image, dataset, project, well, plate, screen, user)
     object_type = script_params["Data_Type"]
     # enter its corresponding ID (except for 'user' : enter the username)
     object_id_list = script_params["IDs"]
 
     today = date.today().strftime("%Y%m%d")
-    key = "Owner_"+today
+    key = "Owner_" + today
 
-    nImage = 0
-    nDataset = 0
-    nProject = 0
-    nWell = 0
-    nPlate = 0
-    nScreen = 0
+    n_image = 0
+    n_dataset = 0
+    n_project = 0
+    n_well = 0
+    n_plate = 0
+    n_screen = 0
     owner = ""
 
     for object_id in object_id_list:
         """ add owner to all objects owned by the specified user"""
-        if(object_type == 'User'):
+        if object_type == 'User':
             # set the group to all
             conn.SERVICE_OPTS.setOmeroGroup('-1')
 
             # get the user
             user = conn.getObject("experimenter", attributes={"omeName": object_id})
 
-            if not user == None:
+            if user is not None:
                 # get the list of groups to search in
                 group_list = []
-                if(conn.getUser().isAdmin()):
+                if conn.getUser().isAdmin():
                     for gem in user.copyGroupExperimenterMap():
                         group_list.append(gem.getParent().getId().getValue())
 
@@ -262,46 +247,46 @@ def add_owner_as_keyval(conn, script_params):
                     # list all user's projects
                     projects = user_conn.getObjects("Project", opts={'owner': user_id})
                     for project in projects:
-                        nImageTmp, nDatasetTmp, nProjectTmp = process_project(user_conn, project, key, owner)
-                        nImage += nImageTmp
-                        nDataset += nDatasetTmp
-                        nProject += nProjectTmp
+                        n_image_tmp, n_dataset_tmp, n_project_tmp = process_project(user_conn, project, key, owner)
+                        n_image += n_image_tmp
+                        n_dataset += n_dataset_tmp
+                        n_project += n_project_tmp
 
                     # lists all user's screens
                     screens = user_conn.getObjects("screen", opts={'owner': user_id})
                     for screen in screens:
-                        nImageTmp, nWellTmp, nPlateTmp, nScreenTmp = process_screen(user_conn, screen, key, owner)
-                        nImage += nImageTmp
-                        nWell += nWellTmp
-                        nPlate += nPlateTmp
-                        nScreen += nScreenTmp
+                        n_image_tmp, n_well_tmp, n_plate_tmp, n_screen_tmp = process_screen(user_conn, screen, key, owner)
+                        n_image += n_image_tmp
+                        n_well += n_well_tmp
+                        n_plate += n_plate_tmp
+                        n_screen += n_screen_tmp
 
                     # lists all user's orpahned dataset
                     orphaned_datasets = user_conn.getObjects("dataset", opts={'owner': user_id, 'orphaned': True})
                     for orphaned_dataset in orphaned_datasets:
-                        nImageTmp, nDatasetTmp = process_dataset(user_conn, orphaned_dataset, key, owner)
-                        nDataset += nDatasetTmp
-                        nImage += nImageTmp
+                        n_image_tmp, n_dataset_tmp = process_dataset(user_conn, orphaned_dataset, key, owner)
+                        n_dataset += n_dataset_tmp
+                        n_image += n_image_tmp
 
                     # lists all user's orpahned plates
                     orphaned_plates = user_conn.getObjects("plate", opts={'owner': user_id, 'orphaned': True})
                     for orphaned_plate in orphaned_plates:
-                        nImageTmp, nWellTmp, nPlateTmp = process_plate(user_conn, orphaned_plate, key, owner)
-                        nImage += nImageTmp
-                        nWell += nWellTmp
-                        nPlate += nPlateTmp
+                        n_image_tmp, n_well_tmp, n_plate_tmp = process_plate(user_conn, orphaned_plate, key, owner)
+                        n_image += n_image_tmp
+                        n_well += n_well_tmp
+                        n_plate += n_plate_tmp
 
                     # lists all user's orpahned images
                     orphaned_images = user_conn.getObjects("image", opts={'owner': user_id, 'orphaned': True})
                     for orphaned_image in orphaned_images:
-                        nImage += process_image(user_conn, orphaned_image, key, owner)
+                        n_image += process_image(user_conn, orphaned_image, key, owner)
 
                 # close the user connection
-                if(conn.getUser().isAdmin()):
+                if conn.getUser().isAdmin():
                     user_conn.close()
 
             else:
-                print("The user",object_id,"does not exists or you do not have access to his/her data")
+                print("The user", object_id, "does not exists or you do not have access to his/her data")
 
         else:
             """ add owner to the specified object and its children"""
@@ -314,8 +299,8 @@ def add_owner_as_keyval(conn, script_params):
             # get the object
             omero_object = conn.getObject(object_type, object_id)
 
-            if not omero_object == None:
-                if(conn.getUser().isAdmin()):
+            if omero_object is not None:
+                if conn.getUser().isAdmin():
                     # get sudo connection
                     user_name = omero_object.getOwner().getOmeName()
                     user_conn = conn.suConn(user_name, ttl=600000)
@@ -324,7 +309,7 @@ def add_owner_as_keyval(conn, script_params):
                 else:
                     user_conn = conn
 
-            if not omero_object == None:
+            if omero_object is not None:
                 # set the correct group Id
                 user_conn.SERVICE_OPTS.setOmeroGroup(omero_object.getDetails().getGroup().getId())
 
@@ -335,52 +320,53 @@ def add_owner_as_keyval(conn, script_params):
 
                 # select object type and add owner as key-value pair
                 if object_type == 'Image':
-                    nImage += process_image(user_conn, omero_object, key, owner)
+                    n_image += process_image(user_conn, omero_object, key, owner)
                 if object_type == 'Dataset':
-                    nImageTmp, nDatasetTmp = process_dataset(user_conn, omero_object, key, owner)
-                    nDataset += nDatasetTmp
-                    nImage += nImageTmp
+                    n_image_tmp, n_dataset_tmp = process_dataset(user_conn, omero_object, key, owner)
+                    n_dataset += n_dataset_tmp
+                    n_image += n_image_tmp
                 if object_type == 'Project':
-                    nImageTmp, nDatasetTmp, nProjectTmp = process_project(user_conn, omero_object, key, owner)
-                    nImage += nImageTmp
-                    nDataset += nDatasetTmp
-                    nProject += nProjectTmp
+                    n_image_tmp, n_dataset_tmp, n_project_tmp = process_project(user_conn, omero_object, key, owner)
+                    n_image += n_image_tmp
+                    n_dataset += n_dataset_tmp
+                    n_project += n_project_tmp
                 if object_type == 'Well':
-                    nImageTmp, nWellTmp = process_well(user_conn, omero_object, key, owner)
-                    nWell += nWellTmp
-                    nImage += nImageTmp
+                    n_image_tmp, n_well_tmp = process_well(user_conn, omero_object, key, owner)
+                    n_well += n_well_tmp
+                    n_image += n_image_tmp
                 if object_type == 'Plate':
-                    nImageTmp, nWellTmp, nPlateTmp = process_plate(user_conn, omero_object, key, owner)
-                    nImage += nImageTmp
-                    nWell += nWellTmp
-                    nPlate += nPlateTmp
+                    n_image_tmp, n_well_tmp, n_plate_tmp = process_plate(user_conn, omero_object, key, owner)
+                    n_image += n_image_tmp
+                    n_well += n_well_tmp
+                    n_plate += n_plate_tmp
                 if object_type == 'Screen':
-                    nImageTmp, nWellTmp, nPlateTmp, nScreenTmp = process_screen(user_conn, omero_object, key, owner)
-                    nImage += nImageTmp
-                    nWell += nWellTmp
-                    nPlate += nPlateTmp
-                    nScreen += nScreenTmp
+                    n_image_tmp, n_well_tmp, n_plate_tmp, n_screen_tmp = process_screen(user_conn, omero_object, key, owner)
+                    n_image += n_image_tmp
+                    n_well += n_well_tmp
+                    n_plate += n_plate_tmp
+                    n_screen += n_screen_tmp
 
                 # close the user connection                
-                if(conn.getUser().isAdmin()):
+                if conn.getUser().isAdmin():
                     user_conn.close()
 
             else:
                 print(object_type, object_id, "does not exist or you do not have access to it")
 
     # build summary message        
-    if(owner == ""):
+    if owner == "":
         message = "Owner cannot be added"
     else:
-        message = "Added {} as owner to {} image(s), {} dataset(s), {} project(s), {} well(s), {} plate(s), {} screen(s) ".format(owner, nImage, nDataset, nProject, nWell, nPlate, nScreen)
+        message = f"Added {owner} as owner to {n_image} image(s), {n_dataset} dataset(s), {n_project} " \
+                  "project(s), {n_well} well(s), {n_plate} plate(s), {n_screen} screen(s) "
     print(message)
 
     return message
 
 
 def run_script():
-
-    data_types = [rstring('Image'), rstring('Dataset'),rstring('Project'), rstring('Well'), rstring('Plate'),rstring('Screen'), rstring('User')]
+    data_types = [rstring('Image'), rstring('Dataset'), rstring('Project'), rstring('Well'), rstring('Plate'),
+                  rstring('Screen'), rstring('User')]
     client = scripts.client(
         'Add_Owner_as_KeyVal',
         """
