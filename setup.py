d75d0ecb8674c5224ae38196f9c997d69f8496cb
Jean-Marie Burel <j.burel@dundee.ac.uk>
2023-09-12 15:16:42 +0100

    Bump version: 5.7.2 → 5.7.3.dev0

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-version = '5.7.2'
+version = '5.7.3.dev0'
 url = "https://github.com/ome/omero-scripts/"
 
 setup(
