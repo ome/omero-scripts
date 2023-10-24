4f26db0af817846ced058d46cf269fdbc0d0c77f
unknown <tom.boissonnet@hhu.de>
2023-10-24 12:29:07 +0200

    Modified deletion according to KeyVal_import_from_csv logic

--- omero/annotation_scripts/Remove_KeyVal.py
+++ omero/annotation_scripts/Remove_KeyVal.py
@@ -31,9 +31,17 @@ import omero
 from omero.rtypes import rlong, rstring, wrap
 import omero.scripts as scripts
 
+HIERARCHY_OBJECTS = {
+                        "Project": ["Dataset", "Image"],
+                        "Dataset": ["Image"],
+                        "Screen": ["Plate", "Well", "Image"],
+                        "Plate": ["Well", "Image"],
+                        #"Run": ["Well", "Image"],
+                        "Well": ["Image"]
+                    }
 
-def remove_map_annotations(conn, obj):
-    anns = list(obj.listAnnotations())
+def remove_map_annotations(conn, obj, namespace):
+    anns = list(obj.listAnnotations(ns=namespace))
     mapann_ids = [ann.id for ann in anns
                   if isinstance(ann, omero.gateway.MapAnnotationWrapper)]
     if len(mapann_ids) == 0:
@@ -46,49 +54,50 @@ def remove_map_annotations(conn, obj):
     except Exception:
         print("Failed to delete links")
         return 1
-    return
 
 
-def get_objects(conn, script_params):
+def get_children_recursive(source_object, target_type):
+    if HIERARCHY_OBJECTS[source_object.OMERO_CLASS][0] == target_type: # Stop condition, we return the source_obj children
+        return source_object.listChildren()
+    else:
+        result = []
+        for child_obj in source_object.listChildren():
+            # Going down in the Hierarchy list for all childs that aren't yet the target
+            result.extend(get_children_recursive(child_obj, target_type))
+        return result
+
+def remove_keyvalue(conn, script_params):
     """
     File the list of objects
     @param conn:             Blitz Gateway connection wrapper
     @param script_params:     A map of the input parameters
     """
-    # we know script_params will have "Data_Type" and "IDs" since these
-    # parameters are not optional
-    data_type = script_params["Data_Type"]
-    ids = script_params["IDs"]
+    source_type = script_params["Source_object_type"]
+    target_type = script_params["Target_object_type"]
+    source_ids = script_params["Source_IDs"]
+    namespace = script_params["Namespace (leave blank for default)"]
 
-    # data_type is 'Dataset', 'Plate' or 'Image' so we can use it directly in
-    objs = list(conn.getObjects(data_type, ids))
-
-    if len(objs) == 0:
-        print("No {} found for specified IDs".format(data_type))
-        return
-
-    objs_ret = []
-
-    if data_type == 'Dataset':
-        for ds in objs:
-            print("Processing Images from Dataset: {}".format(ds.getName()))
-            objs_ret.append(ds)
-            imgs = list(ds.listChildren())
-            objs_ret.extend(imgs)
-    elif data_type == "Plate":
-        for plate in objs:
-            print("Processing Wells and Images from Plate:", plate.getName())
-            objs_ret.append(plate)
-            for well in plate.listChildren():
-                objs_ret.append(well)
-                for ws in well.listChildren():
-                    img = ws.getImage()
-                    objs_ret.append(img)
+    nfailed = 0
+    ntotal = 0
+    if source_type == target_type: # We remove annotation to the given object ID
+        for source_object in conn.getObjects(source_type, source_ids):
+            print("Processing object:", source_object)
+            ret = remove_map_annotations(conn, source_object, namespace)
+            nfailed = nfailed + ret
+            ntotal += 1
     else:
-        print("Processing Images identified by ID")
-        objs_ret = objs
+        for source_object in conn.getObjects(source_type, source_ids):
+            # Listing all target children to the source object (eg all images (target) in all datasets of the project (source))
+            target_obj_l = get_children_recursive(source_object, target_type)
+            for target_obj in target_obj_l:
+                print("Processing object:", target_obj)
+                ret = remove_map_annotations(conn, target_obj, namespace)
+                nfailed = nfailed + ret
+                ntotal += 1
 
-    return objs_ret
+    message = f"Key value data deleted from {ntotal-nfailed} of {ntotal} objects"
+
+    return message
 
 
 if __name__ == "__main__":
@@ -97,7 +106,9 @@ if __name__ == "__main__":
     scripting service, passing the required parameters.
     """
 
-    data_types = wrap(['Dataset', 'Plate', 'Image'])
+    data_types = [rstring("Project"), rstring("Dataset"),
+                  rstring("Screen"), rstring("Plate"),
+                  rstring("Well"), rstring("Image")]
 
     # Here we define the script name and description.
     # Good practice to put url here to give users more guidance on how to run
@@ -110,21 +121,33 @@ if __name__ == "__main__":
          "scripts/user-guide.html for the tutorial that uses this script."),
 
         scripts.String(
-            "Data_Type", optional=False, grouping="1",
-            description="The data you want to work with.", values=data_types,
-            default="Dataset"),
+            "Source_object_type", optional=False, grouping="1",
+            description="Choose the object type containing the objects to delete annotation from",
+            values=data_types, default="Image"),
 
         scripts.List(
-            "IDs", optional=False, grouping="2",
-            description="List of Dataset IDs or Image IDs").ofType(rlong(0)),
+            "Source_IDs", optional=False, grouping="1.1",
+            description="List of source IDs").ofType(rlong(0)),
 
-        authors=["Christian Evenhuis", "MIF"],
-        institutions=["University of Technology Sydney"],
-        contact="https://forum.image.sc/tag/omero"
+        scripts.String(
+            "Target_object_type", optional=False, grouping="2",
+            description="Choose the object type to delete annotation from",
+            values=data_types, default="Image"),
+
+        scripts.String(
+            "Namespace (leave blank for default)", optional=True, grouping="3",
+            description="Choose a namespace for the annotations"),
+
+        authors=["Christian Evenhuis", "MIF", "Tom Boissonnet"],
+        institutions=["University of Technology Sydney", "CAi HHU"],
+        contact="https://forum.image.sc/tag/omero",
+        version="2.0.0"
     )
 
     try:
-        script_params = {}
+        script_params = {
+            "Namespace (leave blank for default)": omero.constants.metadata.NSCLIENTMAPANNOTATION
+        }
         for key in client.getInputKeys():
             if client.getInput(key):
                 # unwrap rtypes to String, Integer etc
@@ -136,19 +159,7 @@ if __name__ == "__main__":
         conn = BlitzGateway(client_obj=client)
 
         # do the editing...
-        objs = get_objects(conn, script_params)
-
-        nfailed = 0
-        for obj in objs:
-            print("Processing object:", obj)
-            ret = remove_map_annotations(conn, obj)
-            nfailed = nfailed + ret
-
-        # now handle the result, displaying message and returning image if
-        # appropriate
-        nobjs = len(objs)
-        message = "Key value data deleted from  {} of {} objects".format(
-                nobjs-nfailed, nobjs)
+        message = remove_keyvalue(conn, script_params)
         client.setOutput("Message", rstring(message))
 
     finally:
